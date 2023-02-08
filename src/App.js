ba37fd4c1ce4a9433b8ef206b96fa5e3fab7ba2a
Evgeniy K <evgen.nn.ru@rambler.ru>
2023-02-08 13:24:53 +0300

    added two-way binding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
-    let likes = 7;
-
-    function increment() {
-      likes += 1;
-      console.log(likes);
+    const [likes, setLikes] = useState(5)
+    const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ')
+   
+    function increment(){
+      setLikes(likes + 1)
     }
 
-    function decrement() {
-      likes -= 1;
-      console.log(likes);
+    function decrement(){
+      setLikes(likes - 1)
     }
 
-
-  return (
-    <div className="App">
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
-  );
+    return (
+      <div className="App">
+        {<h1>{likes}</h1>}
+        {<h1>{value}</h1>}
+        <input 
+            type="text"
+            value={value} 
+            onChange={event => setValue(event.target.value)}
+        />
+        {<button onClick={increment}>Increment</button>}
+        {<button onClick={decrement}>Decrement</button>}
+      </div>
+    );
 }
 
 export default App;
