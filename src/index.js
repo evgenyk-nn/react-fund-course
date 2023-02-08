87eba6ba0b9c2a38754ad642400f73527aef61fd
Evgeniy K <evgen.nn.ru@rambler.ru>
2023-02-08 11:22:55 +0300

    redrawing the component

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
+
+
+
+// ReactDOM.render(
+//   <div>
+//       Приложение работает!
+//       <button>Кнопка</button> 
+//   </div>,
+//   document.getElementById('root')
+// );
