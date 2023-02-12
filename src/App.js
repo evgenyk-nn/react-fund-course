32461c97ae2dd367e82c84e15c5bd50a9a585ee8
Evgeniy K <evgen.nn.ru@rambler.ru>
2023-02-12 16:18:51 +0300

    completion of visual form development

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, {useState} from 'react';
 // import ClassCounter from './components/ClassesCounter';
+import './styles/App.css';
 import Counter from './components/Counter';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
-    const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ')
+  const [posts, setPosts] = useState([
+    {id:1, title: 'Javascript', body: 'Description'},
+    {id:2, title: 'Javascript-1', body: 'Description'},
+    {id:3, title: 'Javascript-2', body: 'Description'},
+   ])
    
+    // const [posts2, setPosts2] = useState([
+    //   {id:1, title: 'Python', body: 'Description'},
+    //   {id:2, title: 'Python-1', body: 'Description'},
+    //   {id:5, title: 'Python-2', body: 'Description'},
+    // ])
+
     // function increment(){
     //   setLikes(likes + 1)
     // }
@@ -13,26 +28,16 @@ function App() {
     //   setLikes(likes - 1)
     // }
 
-    return (
-      <div className="App">
-        <div className='post'>
-          <div className='post_content'>
-            <strong>1. Javascript</strong>
-            <div>
-              Javascript - язык программирования
-            </div>
-          </div>
-          <div className='post_btns'>
-            <button>Удалить</button>
-
-          </div>
-
-
-        </div>
-        <Counter/>
-
-      </div>           
-    );
+  return (
+    <div className="App"> 
+      <form>
+        <MyInput type="text" placeholder="Название поста"/>
+        <MyInput type="text" placeholder="Описание поста"/>
+        <MyButton disabled> Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Список постов про JS"/>
+    </div>           
+  );
 }
 
 export default App;
