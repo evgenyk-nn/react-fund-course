ddc096388d3e8c8afaf37926cee6a311dcafd1bc
Evgeniy K <evgen.nn.ru@rambler.ru>
2023-02-12 21:54:13 +0300

    added the implementation of creating posts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 // import ClassCounter from './components/ClassesCounter';
 import './styles/App.css';
 import Counter from './components/Counter';
@@ -6,15 +6,17 @@ import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
+
 
 function App() {
   const [posts, setPosts] = useState([
     {id:1, title: 'Javascript', body: 'Description'},
     {id:2, title: 'Javascript-1', body: 'Description'},
     {id:3, title: 'Javascript-2', body: 'Description'},
-   ])
+  ])
    
-    // const [posts2, setPosts2] = useState([
+  // const [posts2, setPosts2] = useState([
     //   {id:1, title: 'Python', body: 'Description'},
     //   {id:2, title: 'Python-1', body: 'Description'},
     //   {id:5, title: 'Python-2', body: 'Description'},
@@ -26,15 +28,21 @@ function App() {
 
     // function decrement(){
     //   setLikes(likes - 1)
-    // }
+  // }
+
+  // const [post, setPost] = useState({title: '', body: ''}) 
+  // const [title, setTitle] = useState('')
+  // const [body, setBody] = useState('')
+  // const bodyInputRef = useRef();
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
 
   return (
     <div className="App"> 
-      <form>
-        <MyInput type="text" placeholder="Название поста"/>
-        <MyInput type="text" placeholder="Описание поста"/>
-        <MyButton disabled> Создать пост</MyButton>
-      </form>
+      <PostForm create={createPost}/>
       <PostList posts={posts} title="Список постов про JS"/>
     </div>           
   );
