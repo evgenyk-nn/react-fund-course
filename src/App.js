caf2eeffe377861a68e505c65f15ba620aa513ee
Evgeniy K <evgen.nn.ru@rambler.ru>
2023-02-16 17:10:15 +0300

    added own hooks

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostForm from './components/PostForm';
 import MySelect from './components/UI/select/MySelect';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
+import { useSortedPosts } from './hooks/usePosts';
 
 
 function App() {
@@ -21,18 +22,11 @@ function App() {
   
   const [filter, setfilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = useSortedPosts(posts, filter.sort, filter.query);
 
-  const sortedPosts = useMemo(() => {
-    console.log('ОТРАБОИАЛА ФУНКЦИЯ СОРТЕД ПОСТ')
-    if(filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
+  
 
-  const sortedAndSearchedPosts = useMemo (() =>{
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
