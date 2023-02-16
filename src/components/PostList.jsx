ebe1f1f0fba416eed2ad717a2741948eb26b5aa7
Evgeniy K <evgen.nn.ru@rambler.ru>
2023-02-16 16:45:11 +0300

    added animation (react transition group)

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,30 @@
+// import React, { useState, createRef } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import {
+//   Container,
+//   ListGroup,
+//   Button,
+// } from 'react-bootstrap';
+// import {
+//   CSSTransition,
+//   TransitionGroup,
+// } from 'react-transition-group';
+// import uuid from 'uuid';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+
 import React from 'react';
 import PostItem from './PostItem';
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 
 const PostList = ({posts, title, remove}) => {
 
     if(!posts.length) {
         return (
-            <h1 style={{textAlign: 'center'}}>
+            <h1 style={{textAlign: 'center', color: 'teal'}}>
                 Посты не найдены!
             </h1>
         )
@@ -16,9 +35,17 @@ const PostList = ({posts, title, remove}) => {
             <h1 style={{textAlign: 'center'}}>
                 {title}
             </h1>
-            {posts.map((post, index) =>
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
-            )}      
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>                  
         </div>
     );
 };
